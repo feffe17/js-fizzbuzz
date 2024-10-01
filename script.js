37900433002c9323b682303e71dcb93feb2b6b8c
feffe1702 <fpm.asd@gmail.com>
2024-10-01 18:47:39 +0200

    fix the script with suggestions

--- script.js
+++ script.js
@@ -1,23 +1,35 @@
 // Scrivi un programma che stampi i numeri da 1 a 100, ma per i multipli di 3 stampi “Fizz” al posto del numero e per 
 // i multipli di 5 stampi Buzz. Per i numeri che sono sia multipli di 3 che di 5 stampi FizzBuzz.
-let fizz = "Fizz"
-let buzz = "Buzz"
-let fizzbuzz = "Fizzbuzz"
+const fizz = "Fizz"
+const buzz = "Buzz"
+const fizzbuzz = "Fizzbuzz"
 
-for(let i=1 ; i <=100 ; i++){
-    if((i % 3) == 0){
-        if((i % 5) == 0){
-            console.log(fizzbuzz);
-            
-        } else {
-            console.log(fizz);
-            
-        }
-    }else if((i % 5) == 0){
+// for(let i=1 ; i <=100 ; i++){
+//     if((i % 3) == 0){
+//         if((i % 5) == 0){
+//             console.log(fizzbuzz);
+
+//         } else {
+//             console.log(fizz);
+
+//         }
+//     }else if((i % 5) == 0){
+//         console.log(buzz);
+
+//     } else {
+//         console.log(i);
+
+//     }
+// }
+
+for (let i = 1; i <= 100; i++) {
+    if ((i % 3) == 0 && (i % 5) == 0) {
+        console.log(fizzbuzz);
+    } else if ((i % 3) == 0) {
+        console.log(fizz);
+    } else if ((i % 5) == 0) {
         console.log(buzz);
-        
     } else {
         console.log(i);
-        
     }
-}
+} 
