7e35bf585c2f46e90f3a31c8128c7fa43988e399
feffe1702 <fpm.asd@gmail.com>
2024-10-02 10:26:56 +0200

    add a version with switch case

--- script.js
+++ script.js
@@ -33,3 +33,20 @@ for (let i = 1; i <= 100; i++) {
         console.log(i);
     }
 } 
+
+for (let i = 1; i <= 100; i++){
+    switch (true){
+        case i % 15 == 0:
+        console.log(fizzbuzz);
+        break;
+        case i % 3 == 0:
+        console.log(fizz);
+        break;
+        case i % 5 == 0:
+        console.log(buzz);
+        break;
+    default:
+        console.log(i);
+        break; 
+    }
+}
